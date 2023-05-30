b83717be6941763b7fdef7f653ccf222a93b2351
2ten <m@2ten.in>
2023-05-30 12:46:37 -0400

    1 file modified, 1 added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 export let data
 import Header from '$lib/components/Header.svelte';
 import Footer from '$lib/components/Footer.svelte';
+import '/src/styles//main.css'
 </script>
 
 <Header data={data}/>
