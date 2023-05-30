85a6d681db7affb0707e4f180d54adf1cdfdd5c4
2ten <m@2ten.in>
2023-05-30 16:56:18 -0400

    Build out main nav mobile and desktop versions

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,31 +1,124 @@
 <script>
 	export let data;
 	import * as prismicH from "@prismicio/helpers";
+	import { page } from '$app/stores';
 	let menuItems = [];
 	const mainMenu = data.document.find((item) => item.uid === 'main-menu');
 	if (mainMenu && mainMenu.data.items) {
 		menuItems = mainMenu.data.items
 	}
+	let active = false;
+	$: $page.url && (active = false)
 </script>
 <header class="main-header">
-	<div class="container mx-auto">
-		<div class="header-flex flex items-center justify-between">
-			<div class="logo">Logo</div>
-			<nav class="bg-white flex flex-wrap items-center justify-between p-4">
-				{#if menuItems}
-					{#each menuItems as item}
-						<a href={prismicH.asLink(item.link)} class="text-gray-700 font-semibold uppercase transition-all duration-300 hover:text-gray-100 hover:bg-gray-600 hover:border-transparent rounded-lg px-4 py-2 m-1">
-							{item.name}
-						</a>
-					{/each}
-				{/if}
-			</nav>
-		</div>
+	<div class="flex items-center justify-between">
+		<div class="logo">Logo</div>
+		<a href="#" 
+			 class="main-nav--toggle"
+			 data-label-active="Close"
+			 data-label="Menu"
+			 class:active
+			 on:click={() => active = !active}>Menu</a>
+		<nav class="main-nav">
+			{#if menuItems}
+			<ul class="main-nav--list">
+				{#each menuItems as item}
+					{@const activeLink = item.link.url === $page.url.pathname}
+					<li class="main-nav--item">
+					{#if activeLink}
+						<span class="main-nav--link">{item.name}</span>
+					{:else}
+						<a href={prismicH.asLink(item.link)} class="main-nav--link">{item.name}</a>
+					{/if}
+					</li>
+				{/each}
+			</ul>
+			{/if}
+		</nav>
 	</div>
 </header>
 <style>
 .main-header{
 	border-bottom:solid 1px #ccc;
+	padding-left:15px;
+	padding-right:15px;
+}
+.main-nav--list{
+	margin:0;
+	padding:0;
+	list-style:none;
+}
+@media(max-width:1024px){
+	.main-nav{
+		position:fixed;
+		background:black;
+		top:0;
+		left:0;
+		right:0;
+		bottom:0;
+		width:100%;
+		height:100%;
+		z-index:10;
+		pointer-events:none;
+		opacity:0;
+		transition:opacity 0.2s ease;
+	}
+	.main-nav--toggle{
+
+	}
+	.main-nav--toggle.active{
+		color:white;
+		z-index:11;
+	}
+	.main-nav--toggle.active + .main-nav{
+		pointer-events:all;
+		opacity:1;
+	}
+	.main-nav--list{
+		margin-top:30px;
+	}
+	.main-nav--item{
+
+	}
+	.main-nav--link{
+		padding:10px 15px;
+		color:white;
+		display:block;
+		text-align:center;
+		font-size:1.5em;
+	}
+	span.main-nav--link{
+		opacity:0.5;
+	}
+}
+@media(min-width:1025px){
+	.main-header{
+		padding-left:30px;
+		padding-right:30px;
+	}
+	.main-nav--toggle{
+		display:none;
+	}
+	.main-nav{
+		display:flex;
+		align-items:center;
+		justify-content:space-between;
+	}
+	.main-nav--list{
+		
+	}
+	.main-nav--item{
+		display:inline-block;
+	}
+	.main-nav--link{
+		padding:5px 8px;
+		color:black;
+		font-size:1em;
+		display:block;
+	}
+	span.main-nav--link{
+		opacity:0.5;
+	}
 }
 .logo{
 	font-weight:bold;
