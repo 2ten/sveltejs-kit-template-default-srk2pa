10543ade7daf021582d2e6a1dc35bc252ee64f02
2ten <m@2ten.in>
2023-06-07 17:40:56 -0400

    BlogItem component

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,6 +2,7 @@
   export let data;
   import { SliceZone } from '@prismicio/svelte';
   import Pagination from '$lib/components/Pagination.svelte';
+  import BlogItem from '$lib/components/BlogItem.svelte';
   import { dev } from "$app/environment"
   import * as components from "$lib/slices"
 	import * as prismic from '@prismicio/client';
@@ -23,26 +24,7 @@
   <div class="blog-grid">
     {#if data && data.documents.blogs.results}
       {#each data.documents.blogs.results as blog}
-
-        <div class="blog-item">
-          <a href={prismic.asLink(blog)} class="blog-item--link">
-          {#if blog.data.image.url}
-          <div class="blog-item--image">
-            <div class="image-container">
-              <img  
-			          src={prismic.asImageSrc(blog.data.image)}
-			          srcset={prismic.asImageWidthSrcSet(blog.data.image).srcset} 
-			          alt={blog.data.image.alt} />
-            </div>
-          </div>
-          {/if}
-          <div class="blog-item--caption">
-            {#if blog.data.title[0] && blog.data.title[0].text}<h3>{@html prismic.asText(blog.data.title)}</h3>{/if}
-            {#if blog.data.description[0] && blog.data.description[0].text}<p>{@html prismic.asText(blog.data.description)}</p>{/if}
-          </div>
-          </a>
-        </div>
-
+        <BlogItem {blog} />
       {/each}
     {/if}
   </div>
@@ -54,40 +36,14 @@
   display: grid;
   gap: 20px;
 }
-.blog-item {
-  width: 100%;
-}
-.blog-item--link{
-  display:block;
-}
-.blog-item--image .image-container {
-  aspect-ratio: 3/2;
-}
-.blog-item--image img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-.blog-item--caption h3 {
-  margin: 10px 0 5px;
-}
-.blog-item--caption p {
-  margin: 0;
-}
 @media (min-width: 640px) {
   .blog-grid {
     grid-template-columns: repeat(2, 1fr);
-  }
-  .blog-item {
-    margin-bottom: 20px;
   }
 }
 @media (min-width: 960px) {
   .blog-grid {
     grid-template-columns: repeat(3, 1fr);
-  }
-  .blog-item {
-    margin-bottom: 40px;
   }
 }
 </style>
